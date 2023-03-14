4aea6482931ae2fbeb99566d3abf38403bc28223
Eduard <ed.kech2005@mail.ru>
2023-03-14 19:03:33 -0400

    tree works like i always wanted it to

--- lua/tree.lua
+++ lua/tree.lua
@@ -8,9 +8,30 @@ require("nvim-tree").setup({
   },
 })
 
-local api = require('nvim-tree.api')
+local tree_api = require('nvim-tree.api')
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<cr>')
-vim.keymap.set('n', 'c', api.fs.copy.relative_path)
--- vim.keymap.set('n', 'n', api.fs.create)
--- vim.keymap.set('n', 'r', api.fs.rename)
-
+vim.keymap.set('n', 'c', tree_api.fs.copy.relative_path)
+vim.keymap.set('n', 'r', function()
+  if vim.bo.filetype == 'NvimTree' then
+    tree_api.fs.rename()
+  end
+end)
+vim.keymap.set('n', 'd', function()
+  if vim.bo.filetype == 'NvimTree' then
+    tree_api.fs.delete()
+  end
+end)
+vim.keymap.set('n', 'n', function()
+  if vim.bo.filetype == 'NvimTree' then
+    tree_api.fs.create()
+  end
+end)
+-- make a keymap which unfocusses tree if inside the tree 
+-- and focuses on tree if not focused
+vim.keymap.set('n', '<leader>E', function()
+  if vim.bo.filetype == 'NvimTree' then
+    vim.cmd('wincmd p')
+  else
+    vim.cmd('NvimTreeFocus')
+  end
+end)
