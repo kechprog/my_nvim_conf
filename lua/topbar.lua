0c2315ed8b1b4dadcc7e783a2f6c26cd0bde224a
Eduard <ed.kech2005@mail.ru>
2023-04-14 16:29:10 -0400

    new api in barbar

--- lua/topbar.lua
+++ lua/topbar.lua
@@ -1,47 +1,49 @@
 require 'bufferline'.setup {
-  animation               = false,
-  auto_hide               = false,
-  tabpages                = true,
-  closable                = true,
-  clickable               = true,
+  animation = false,
+  auto_hide = false,
+  tabpages  = true,
+  closable  = true,
+  clickable = true,
 
-  diagnostics             = {
-    -- you can use a list
-    { enabled = true, icon = 'ﬀ' }, -- ERROR
-    { enabled = false }, -- WARN
-    { enabled = false }, -- INFO
-    { enabled = true }, -- HINT
 
-    [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
-    [vim.diagnostic.severity.WARN] = { enabled = false },
-    [vim.diagnostic.severity.INFO] = { enabled = false },
-    [vim.diagnostic.severity.HINT] = { enabled = true },
+
+  icons              = {
+    filetype    = { enabled = true },
+    diagnostics = {
+      -- you can use a list
+      { enabled = true, icon = 'ﬀ' }, -- ERROR
+      { enabled = false },          -- WARN
+      { enabled = false },          -- INFO
+      { enabled = true },           -- HINT
+      [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+      [vim.diagnostic.severity.WARN] = { enabled = false },
+      [vim.diagnostic.severity.INFO] = { enabled = false },
+      [vim.diagnostic.severity.HINT] = { enabled = true },
+    },
   },
 
-  icons                   = true,
-
-  icon_custom_colors      = false,
+  icon_custom_colors = false,
 
   -- Configure icons on the bufferline.
-  icon_separator_active   = '▎',
-  icon_separator_inactive = '▎',
-  icon_close_tab          = '',
-  icon_close_tab_modified = '●',
-  icon_pinned             = '車',
+  -- icon_separator_active   = '▎',
+  -- icon_separator_inactive = '▎',
+  -- icon_close_tab          = '',
+  -- icon_close_tab_modified = '●',
+  -- icon_pinned             = '車',
 
   -- If true, new buffers will be inserted at the start/end of the list.
   -- Default is to insert after current buffer.
-  insert_at_start         = true,
-  maximum_padding         = 1,
-  minimum_padding         = 1,
-  maximum_length          = 30,
-  semantic_letters        = true,
-  letters                 = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
-  no_name_title           = nil,
+  insert_at_start    = true,
+  maximum_padding    = 1,
+  minimum_padding    = 1,
+  maximum_length     = 30,
+  semantic_letters   = true,
+  letters            = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+  no_name_title      = nil,
 }
 
-local opts = {silent = true}
+local opts = { silent = true }
 local map = vim.keymap.set
 map('n', 'H', ':BufferPrevious<CR>', opts)
-map('n', 'L', ':BufferNext<CR>',     opts)
+map('n', 'L', ':BufferNext<CR>', opts)
 map('n', '<leader>c', ':BufferClose<CR>', opts)
