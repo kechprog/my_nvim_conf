75ac550960fd968d99b9b15bb259a999d33b364a
ed <ed.kech2005@mail.ru>
2023-05-09 19:15:44 -0400

    typo

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vim' },
   auto_install = false,
 
   highlight = { enable = true },
