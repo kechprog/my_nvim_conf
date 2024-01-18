9beba784b3dc55c39050170dcc63fb0fd5e61f46
Eduard <ed.kech2005@mail.ru>
2024-01-17 20:37:02 -0500

    scope highlight

--- lua/_ibl.lua
+++ lua/_ibl.lua
@@ -2,5 +2,12 @@ require "ibl".setup({
     indent = {
         tab_char = {'.'},
         char = {'|'},
+        highlight = {"Function", "Label"},
+        priority = 2,
     },
+    scope = {
+        show_start = true,
+        show_end = true,
+        priority = 3,
+    }
 })
