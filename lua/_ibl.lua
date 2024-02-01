85b64a44bb0604f2cbd653ecdabba59b6600df6c
Eduard <ed.kech2005@mail.ru>
2024-01-31 20:09:57 -0500

    ibl

--- lua/_ibl.lua
+++ lua/_ibl.lua
@@ -1,13 +1,1 @@
-require "ibl".setup({
-    indent = {
-        tab_char = {'.'},
-        char = {'|'},
-        highlight = {"Function", "Label"},
-        priority = 2,
-    },
-    scope = {
-        show_start = true,
-        show_end = true,
-        priority = 3,
-    }
-})
+require "ibl".setup({})
