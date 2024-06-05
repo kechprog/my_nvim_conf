11a877a9d05771b4db51b5217e745cfd10e4f56d
Eduard <ed.kech2005@mail.ru>
2024-06-05 01:57:40 -0400

    neovide paste

--- lua/nvide.lua
+++ lua/nvide.lua
@@ -4,16 +4,24 @@ if vim.g.neovide then
         vim.o.guifont = string.format("FiraCode Nerd Font:h%d", font_size)
     end
 
-    vim.keymap.set("n", "<C-+>", function ()
+    local function paste_in_terminal()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-N>\"+pa", true, true, true), "n", true)
+        vim.api.nvim_feedkeys("i", "n", true) -- Return to insert mode in terminal
+    end
+
+
+    vim.keymap.set("n", "<C-+>", function()
         font_size = font_size + 1
         update_font_size()
-    end, {silent=false, desc="Increase font in Neovide"})
+    end, { silent = false, desc = "Increase font in Neovide" })
 
-    vim.keymap.set("n", "<C-_>", function ()
+    vim.keymap.set("n", "<C-_>", function()
         font_size = font_size - 1
         update_font_size()
-    end, {silent=false, desc="Decrease font in Neovide"})
+    end, { silent = false, desc = "Decrease font in Neovide" })
+
+    vim.keymap.set("i", "<C-v>", "<C-r>+", { noremap = true, silent = true })
+    vim.keymap.set("t", "<C-v>", paste_in_terminal, { noremap = true, silent = true })
 
     update_font_size()
 end
-
